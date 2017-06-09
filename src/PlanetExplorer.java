628aa3e3a067ca5d2fc828177c5f32092bfc4017
somename <someemail>
2017-06-09 12:31:06 +0200

    besouro automatic message

--- src/PlanetExplorer.java
+++ src/PlanetExplorer.java
@@ -57,7 +57,7 @@ public class PlanetExplorer {
 		 * The return string should also contain a list of coordinates of the encountered obstacles. No white spaces.
 		 */
 		
-		return null;
+		return "("+koordinataX + "," + koordinataY + "," + smer+")";
 	}
 
 	public int getGranicaX() {
