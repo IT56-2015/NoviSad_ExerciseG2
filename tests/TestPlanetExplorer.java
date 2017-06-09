77596429b52da3c8ab035ae9876e03dcc68ab30c
somename <someemail>
2017-06-09 13:05:50 +0200

    besouro automatic message

--- tests/TestPlanetExplorer.java
+++ tests/TestPlanetExplorer.java
@@ -12,7 +12,7 @@ public class TestPlanetExplorer {
 	@Test
 	public void test_R() throws PlanetExplorerException{
 		PlanetExplorer pe = new PlanetExplorer(3, 3, null);
-		assertEquals("Greska!", "(0,0,n)", pe.executeCommand("r"));
+		assertEquals("Greska!", "(0,0,e)", pe.executeCommand("r"));
 	}
 	@Test
 	public void test_L() throws PlanetExplorerException{
